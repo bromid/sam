23f6fae96e99f5837a89a4883db314157fac8b95
Dennis Granath <dennis.granath@konsult.atg.se>
2016-06-20 07:22:03 +0200

    Two space tab-indentation

--- atg-cmdb-server/src/main/java/se/atg/cmdb/ui/rest/AssetResource.java
+++ atg-cmdb-server/src/main/java/se/atg/cmdb/ui/rest/AssetResource.java
@@ -46,88 +46,88 @@ import se.atg.cmdb.ui.dropwizard.auth.Roles;
 @Produces(Defaults.MEDIA_TYPE_JSON)
 public class AssetResource {
 
-    private static final Logger logger = LoggerFactory.getLogger(AssetResource.class);
-    private static final Bson ALL = new BsonDocument();
+  private static final Logger logger = LoggerFactory.getLogger(AssetResource.class);
+  private static final Bson ALL = new BsonDocument();
 
-    private final MongoDatabase database;
-    private final ObjectMapper objectMapper;
+  private final MongoDatabase database;
+  private final ObjectMapper objectMapper;
 
-    public AssetResource(MongoDatabase database, ObjectMapper objectMapper) {
-        this.database = database;
-        this.objectMapper = objectMapper;
+  public AssetResource(MongoDatabase database, ObjectMapper objectMapper) {
+    this.database = database;
+    this.objectMapper = objectMapper;
+  }
+
+  @GET
+  @Path("services/asset")
+  @RolesAllowed(Roles.READ)
+  @ApiOperation("Fetch all assets")
+  public PaginatedCollection<Asset> getAssets() {
+    return findAssets(ALL);
+  }
+
+  @GET
+  @Path("services/asset/{id}")
+  @RolesAllowed(Roles.READ)
+  @ApiOperation(value = "Fetch an asset", response=Asset.class)
+  public Response getAsset(
+      @ApiParam("id") @PathParam("id") String id
+      ) {
+    final Document asset = findAsset(Filters.eq("id", id));
+    return Response
+        .ok(new Asset(asset))
+        .tag(RESTHelper.getEntityTag(asset).orElse(null))
+        .build();
+  }
+
+  @PUT
+  @Path("services/asset")
+  @RolesAllowed(Roles.EDIT)
+  @ApiOperation(value="Create a new asset", code=201, response=AssetLink.class)
+  public Response createAsset(
+      @ApiParam("Asset") Asset asset,
+      @Context UriInfo uriInfo,
+      @Context SecurityContext securityContext
+  ) throws JsonParseException, JsonMappingException, IOException{
+    logger.info("Create asset: {}", asset);
+
+    RESTHelper.validate(asset, Asset.Create.class);
+
+    final User user = RESTHelper.getUser(securityContext);
+    final Document bson = JSONHelper.addMetaForCreate(asset, user.name, objectMapper);
+    database.getCollection(Collections.ASSETS).insertOne(bson);
+
+    return linkResponse(Status.CREATED, bson, uriInfo);
+  }
+
+  private PaginatedCollection<Asset> findAssets(Bson filter) {
+
+    final MongoCollection<Document> collection = database.getCollection(Collections.ASSETS);
+    final FindIterable<Document> query;
+    if (filter == ALL) {
+      query = collection.find();
+    } else {
+      query = collection.find(filter);
     }
+    return RESTHelper.paginatedList(query.map(Asset::new));
+  }
 
-    @GET
-    @Path("services/asset")
-    @RolesAllowed(Roles.READ)
-    @ApiOperation("Fetch all assets")
-    public PaginatedCollection<Asset> getAssets() {
-        return findAssets(ALL);
+  private Document findAsset(Bson filter) {
+    final Document bson = database.getCollection(Collections.ASSETS)
+        .find(filter)
+        .first();
+    if (bson == null) {
+      throw new WebApplicationException(Status.NOT_FOUND);
     }
+    return bson;
+  }
 
-    @GET
-    @Path("services/asset/{id}")
-    @RolesAllowed(Roles.READ)
-    @ApiOperation(value = "Fetch an asset", response=Asset.class)
-    public Response getAsset(
-            @ApiParam("id") @PathParam("id") String id
-            ) {
-        final Document asset = findAsset(Filters.eq("id", id));
-        return Response
-                .ok(new Asset(asset))
-                .tag(RESTHelper.getEntityTag(asset).orElse(null))
-                .build();
-    }
-
-    @PUT
-    @Path("services/asset")
-    @RolesAllowed(Roles.EDIT)
-    @ApiOperation(value="Create a new asset", code=201, response=AssetLink.class)
-    public Response createAsset(
-            @ApiParam("Asset") Asset asset,
-            @Context UriInfo uriInfo,
-            @Context SecurityContext securityContext
-    ) throws JsonParseException, JsonMappingException, IOException{
-        logger.info("Create asset: {}", asset);
-
-        RESTHelper.validate(asset, Asset.Create.class);
-
-        final User user = RESTHelper.getUser(securityContext);
-        final Document bson = JSONHelper.addMetaForCreate(asset, user.name, objectMapper);
-        database.getCollection(Collections.ASSETS).insertOne(bson);
-
-        return linkResponse(Status.CREATED, bson, uriInfo);
-    }
-
-    private PaginatedCollection<Asset> findAssets(Bson filter) {
-
-        final MongoCollection<Document> collection = database.getCollection(Collections.ASSETS);
-        final FindIterable<Document> query;
-        if (filter == ALL) {
-            query = collection.find();
-        } else {
-            query = collection.find(filter);
-        }
-        return RESTHelper.paginatedList(query.map(Asset::new));
-    }
-
-    private Document findAsset(Bson filter) {
-        final Document bson = database.getCollection(Collections.ASSETS)
-                .find(filter)
-                .first();
-        if (bson == null) {
-            throw new WebApplicationException(Status.NOT_FOUND);
-        }
-        return bson;
-    }
-
-    private Response linkResponse(Status status, Document bson, UriInfo uriInfo) {
-        final AssetLink response = new AssetLink(uriInfo.getBaseUri(), bson.getString("id"), bson.getString("name"));
-        return Response
-                .status(status)
-                .location(response.link.getUri())
-                .entity(response)
-                .tag(RESTHelper.getEntityTag(bson).orElse(null))
-                .build();
-    }
+  private Response linkResponse(Status status, Document bson, UriInfo uriInfo) {
+    final AssetLink response = new AssetLink(uriInfo.getBaseUri(), bson.getString("id"), bson.getString("name"));
+    return Response
+        .status(status)
+        .location(response.link.getUri())
+        .entity(response)
+        .tag(RESTHelper.getEntityTag(bson).orElse(null))
+        .build();
+  }
 }
