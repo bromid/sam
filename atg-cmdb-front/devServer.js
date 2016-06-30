77ecacf4303200c491e290c9963a970c2d01f80a
Kristofer Lindberg <kristofer.lindberg@konsult.atg.se>
2016-06-30 18:04:07 +0200

    Proxy services to dropwizard when running dev server

--- atg-cmdb-front/devServer.js
+++ atg-cmdb-front/devServer.js
@@ -3,15 +3,27 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import config from './webpack.config.babel';
 import Express from 'express';
+import httpProxy from 'http-proxy';
 
 const app = new Express();
+const servicesProxy = httpProxy.createProxyServer();
 const port = 3001;
 
 const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
-    publicPath: config.output.publicPath,
+    publicPath: config.output.publicPath
 }));
+
+app.use("/services/*", function(req, res) {
+    req.url = req.baseUrl;
+    servicesProxy.web(req, res, {
+        target: {
+            port: 8080,
+            host: 'localhost'
+        }
+    });
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -23,7 +35,7 @@ app.listen(port, error => {
         console.error(error);
     } else {
         console.info(
-            '🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.',
+            'Listening on port %s. Open up http://localhost:%s/ in your browser.',
             port,
             port
         );
