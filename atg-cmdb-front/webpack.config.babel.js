b7cccdec78de9402f069d3192e2c804773ad4db5
Markus Ånöstam <markus87@gmail.com>
2016-07-07 10:08:26 +0200

    Fixed lint errors in webpack config

--- atg-cmdb-front/webpack.config.babel.js
+++ atg-cmdb-front/webpack.config.babel.js
@@ -1,9 +1,10 @@
 import path from 'path';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 
-const srcPath = path.join(__dirname, 'src');
-
-var publicPath = "/static/";
+const Paths = {
+    src: path.join(__dirname, 'src'),
+    public: '/static/',
+};
 
 export default {
     devtool: 'eval',
@@ -11,31 +12,31 @@ export default {
     output: {
         path: path.join(__dirname, 'dist/static'),
         filename: 'bundle.js',
-        publicPath: publicPath
+        publicPath: Paths.public,
     },
     plugins: [
         new CopyWebpackPlugin([
             {
-                from: path.join(srcPath, 'icons'),
-                to: 'icons'
-            }
-        ])
+                from: path.join(Paths.src, 'icons'),
+                to: 'icons',
+            },
+        ]),
     ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 loaders: ['babel'],
-                include: srcPath,
+                include: Paths.src,
             }, {
                 test: /\.json$/,
                 loaders: ['json'],
-                include: srcPath,
+                include: Paths.src,
             }, {
                 test: /\.(png|jpg)$/,
-                include: srcPath,
+                include: Paths.src,
                 loader: 'url?name=img/[name]-[hash].[ext]&limit=25000',
-            }
-        ]
-    }
+            },
+        ],
+    },
 };
