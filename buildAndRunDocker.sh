f2ca4d4ea3723a7621cf40098f5e8b70bfb64c6e
Kristofer <kristofer@bromid.se>
2016-07-09 21:53:46 +0200

    Added helper file for building and running with docker for development

---
+++ buildAndRunDocker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mvn clean package -P local -pl \!atg-cmdb-release -T 1.5C || {
+    echo 'Build failed';
+    exit 1;
+}
+
+docker stop cmdb
+docker rm cmdb
+
+set -e
+docker build -t atg/cmdb .
+docker create -p 8080:8080 -p 27017:27017 --name cmdb atg/cmdb
+docker start cmdb
+
+source addTestData.sh
