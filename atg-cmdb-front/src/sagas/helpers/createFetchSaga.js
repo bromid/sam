60446efe47bb6194cb0220e0c4d15c58960fa6e7
Markus Ånöstam <markus87@gmail.com>
2016-07-25 23:05:47 +0200

    Created fetch-saga helper

---
+++ atg-cmdb-front/src/sagas/helpers/createFetchSaga.js
@@ -0,0 +1,37 @@
+import identity from 'lodash/identity';
+import { call, put } from 'redux-saga/effects';
+
+const keyRequired = (key) => { throw new Error(`${key} must be specified!`); };
+
+const getAPIParams = (paramSelector, params) => {
+    const paramsAsArray = [].concat(params);
+    if (!paramSelector) return paramsAsArray;
+
+    return [].concat(paramSelector(...paramsAsArray));
+};
+
+export default function createFetchSaga(options) {
+    const {
+        apiCall = keyRequired('ApiCall'),
+        responseKey = keyRequired('responseKey'),
+        payloadTransform = identity,
+        errorTransform = identity,
+        paramSelector,
+    } = options;
+
+    return function* fetchSaga(...params) {
+        try {
+            const payload = yield call(apiCall, ...getAPIParams(paramSelector, params));
+            yield put({
+                type: responseKey,
+                payload: payloadTransform(payload.data, payload.response),
+            });
+        } catch (error) {
+            yield put({
+                type: responseKey,
+                error: true,
+                payload: errorTransform(error, error.response),
+            });
+        }
+    };
+}
