cd13f205bb74bc4191a252e82d0007f88a6c9055
Markus Ånöstam <markus87@gmail.com>
2016-08-07 10:23:22 +0200

    Use Paths.DIST variable instead of writing path

--- atg-cmdb-front/webpack.config.babel.js
+++ atg-cmdb-front/webpack.config.babel.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import webpack from 'webpack';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import validate from 'webpack-validator';
 
@@ -14,7 +15,7 @@ const config = {
     devtool: 'eval',
     entry: Paths.APP_ENTRY,
     output: {
-        path: path.join(__dirname, 'dist/static'),
+        path: Paths.DIST,
         filename: 'bundle.js',
         publicPath: Paths.PUBLIC,
     },
