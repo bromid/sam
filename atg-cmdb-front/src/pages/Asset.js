c5782a1f2cd1c3db60857ce0f22e2bd80651d1e7
Kristofer Lindberg <kristofer.lindberg@konsult.atg.se>
2016-09-07 16:46:52 +0200

    Added component for assigning asset to a group

--- atg-cmdb-front/src/pages/Asset.js
+++ atg-cmdb-front/src/pages/Asset.js
@@ -1,14 +1,100 @@
 import React from 'react';
-import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
+import * as assetValidators from '../validators/assetValidators';
 import * as assetActions from '../actions/assetActions';
 import LoadingIndicator from '../components/LoadingIndicator';
 import Attributes from '../components/Attributes';
 import ItemView from '../components/ItemView';
-import { fromAsset } from '../reducers';
+import EditableGroupLink from '../components/EditableGroupLink';
+import { State } from '../components/EditState';
+import * as StateMachine from '../components/EditStateMachine';
+import { fromAsset, fromGroup, getAuthenticated } from '../reducers';
+
+const groupId = (group) => (
+    (group) ? group.id : ''
+);
+
+const Details = ({ groupLink, attributes }) => (
+    <div>
+        <dl>
+            <dt>Group</dt>
+            {groupLink}
+        </dl>
+        <Attributes attributes={attributes} />
+    </div>
+);
+
+const Asset = (props) => {
+    const {
+        asset: { name, description = '', attributes, meta },
+        isLoading, patchIsPending, patchError, updateName, updateDescription, groupLink,
+    } = props;
+
+    const tabs = [
+        {
+            name: 'Details',
+            node: (
+                <Details groupLink={groupLink} attributes={attributes} />
+            ),
+        },
+    ];
+
+    return (
+        <ItemView
+            tabs={tabs}
+            headline={name}
+            updateHeadline={updateName}
+            validateHedline={assetValidators.name}
+            description={description}
+            updateDescription={updateDescription}
+            validateDescription={assetValidators.description}
+            meta={meta}
+            isLoading={isLoading}
+            patchIsPending={patchIsPending}
+            patchError={patchError}
+        />
+    );
+};
 
 const AssetContainer = React.createClass({
+
+    getInitialState() {
+        const { authenticated, asset: { group } = { } } = this.props;
+        return {
+            group: groupId(group),
+            groupErrorText: '',
+            groupState: StateMachine.mapState(authenticated, this.updateGroup),
+        };
+    },
+
+    componentWillReceiveProps(newProps) {
+        const {
+            authenticated, patchError, patchIsPending,
+            asset: { group } = { },
+        } = newProps;
+        const { groupState } = this.state;
+
+        const error = StateMachine.parseError(patchError);
+
+        const newGroupState = StateMachine.mapStateFromCurrent(
+            groupState,
+            authenticated,
+            this.updateGroup,
+            patchIsPending,
+            error.isError
+        );
+
+        this.setState({
+            ...StateMachine.newPropsState(
+                'group',
+                newGroupState,
+                State.readonly,
+                groupId(group),
+                error
+            ),
+        });
+    },
 
     updateName(name) {
         const { patchAsset, asset: { id, meta } } = this.props;
@@ -20,37 +106,61 @@ const AssetContainer = React.createClass({
         patchAsset(id, { description }, { hash: meta.hash });
     },
 
+    editGroup(edit = true) {
+        const { authenticated } = this.props;
+        const groupState = StateMachine.mapState(authenticated, this.updateGroup, edit);
+        this.setState({ groupState });
+    },
+
+    cancelEditGroup() {
+        const { asset: { group } = {} } = this.props;
+        this.setState({ group: groupId(group), groupErrorText: '' });
+        this.editGroup(false);
+    },
+
+    changeGroup(group) {
+        const value = group.trim().toLocaleLowerCase();
+        const errorText = assetValidators.group(value);
+        this.setState(StateMachine.changeState('group', value, errorText));
+    },
+
+    updateGroup(event) {
+        event.preventDefault();
+        const { patchAsset, asset: { id, meta } } = this.props;
+        const { group, groupState } = this.state;
+        if (groupState !== State.validationFailed) {
+            this.editGroup(false);
+            patchAsset(id, { group }, { hash: meta.hash });
+        }
+    },
+
     render() {
-        const { asset, isLoading, patchIsPending, patchError } = this.props;
+        const { asset, groupIds, isLoading, patchIsPending, patchError } = this.props;
 
         if (isLoading && isEmpty(asset)) return <LoadingIndicator />;
 
-        const { name, description = '', group, attributes, meta } = asset;
-        const tabs = [
-            {
-                name: 'Details',
-                node: (
-                    <div>
-                        <dl>
-                            <dt>Group</dt>
-                            <dd>{group && <Link to={`/group/${group.id}`}>{group.name}</Link>}</dd>
-                        </dl>
-                        <Attributes attributes={attributes} />
-                    </div>
-                ),
-            },
-        ];
+        const { group, groupErrorText, groupState } = this.state;
+        const groupLink = (
+            <EditableGroupLink
+                group={asset.group}
+                groupIds={groupIds}
+                value={group}
+                errorText={groupErrorText}
+                state={groupState}
+                edit={this.editGroup}
+                change={this.changeGroup}
+                cancel={this.cancelEditGroup}
+                save={this.updateGroup}
+            />);
         return (
-            <ItemView
-                headline={name}
-                updateHeadline={this.updateName}
-                description={description}
-                updateDescription={this.updateDescription}
-                meta={meta}
-                tabs={tabs}
+            <Asset
+                asset={asset}
                 isLoading={isLoading}
                 patchIsPending={patchIsPending}
                 patchError={patchError}
+                groupLink={groupLink}
+                updateDescription={this.updateDescription}
+                updateName={this.updateName}
             />
         );
     },
@@ -62,7 +172,9 @@ const mapStateToProps = (state) => ({
     patchResult: fromAsset.getPatchResult(state),
     patchError: fromAsset.getPatchResultError(state),
     patchIsPending: fromAsset.getPatchResultIsPending(state),
+    groupIds: fromGroup.getIds(state),
     isLoading: fromAsset.getCurrentIsPending(state),
+    authenticated: getAuthenticated(state),
 });
 
 const Actions = {
